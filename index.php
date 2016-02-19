3ab7a6559db7d9e94d9d06b97948712630131947
Bovsik91 <Bovsik91@mail.ru>
2016-02-19 20:48:33 +0200

    site

---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ремонт ПК</title>
+<link type="text/css" rel="stylesheet" href="main.css"> 
+<!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
+<!--<meta charset="windows-1251">  -->
+<!-- <base href="http://"> -->
+</head>
+
+<body>
+<div class="maket">
+<header>
+			<div style="border:1px solid red; height:235px">
+			<h2 class="activity">Настройка компьютеров/ноутбуков</h2>
+            <h3>Контакты<br />Телефоны<br />Логотип</h3>
+			</div>
+		<nav>
+	<ul id="menu">
+		<li><a href="Ремонт ПК.html" class="but1">Главная</a></li>
+		<li>
+				<a href="#" class="but2">Компьютерные услуги</a>
+		
+			<ul>
+				<li>
+				<a href="#">Настройка компьютера</a>
+				<a href="#">Настройка ноутбука</a>
+				<a href="#">Установка Windows</a>
+				<a href="#">Удаление вирусов и установка защиты</a>
+				<a href="#">Обслуживание компьютеров</a>
+				</li>
+			</ul> 
+		</li>
+		
+		<li><a href="#" class="but3_active">Установка Windows</a></li>
+		<li><a href="#" class="but4">Чистка от вирусов</a></li>
+		<li><a href="#" class="but5">Контакты</a></li>
+		<li><a href="#" class="but6">Цены</a></li>
+	</ul>
+		</nav>
+</header>
+
+<div class="menu_add">Строчная реклама</div>
+
+  <div class="vnutr_maket">
+
+
+<aside>Рекламма1
+<div><img src="images/04.jpg"></div>
+</aside>
+
+<main>
+
+
+<div class="header3">
+<h1>Установка Windows Киев. Переустановка ОС в Киеве</h1>
+<H3>Стоимость установки Windows, всех программ и драйверов, включая последние версии антивируса, составляет 200 грн.</H3>
+<H4>Вызов мастера: <br /><br />
+(097) xxx-xx-xx, (063) xxx-xx-xx
+</H4>
+</div>
+ 
+
+<div class="images"><img src="images/01.jpg" style="width:342px; height:250px"></div>
+<div id="text">
+
+Операционная система – это самая важная программа, которая устанавливается на каждый компьютер, без нее мы не смогли бы даже включить компьютер.
+работа компьютера зависит от того, какая версия и как будет установлена операционная система.
+Поэтому настройка Windows должна осуществляться только профессионалами, т.к настроить Windows возможна только, если знаешь особенности системы и компьютера, а также нюансы настройки Windows.
+
+<div>
+<ol><b>Полная установка Windows программы включает:</b>
+ <div>&nbsp;</div>
+	<li>Сохранение всех пользовательских данных;</li>
+	<li>Форматирование жесткого диска;</li>
+	<li>Установку и настройку Windows, антивирусных программ, систем безопасности, кодеков и драйверов;</li>
+	<li>Подсоединение к сети Интернет, восстановление сетевых настроек, которые были ранее на ПК;</li>
+	<li>Установку утилит и дополнительных программ.</li>
+</ol>
+</div>
+<div>
+<u>Когда нужна установка/переустановка Windows?</u>
+<div>&nbsp;</div>
+</div>
+<div>
+Ноутбук или компьютер странно себя ведет: тормозит, Windows выдает множество ошибок, не загружается, самопроизвольно начинает перезагружаться. Все это признаки того, что ваша операционная система работает со сбоями.
+<div>&nbsp;</div>
+</div>
+<div>
+<u>Почему могут возникать ошибки в работе Windows?</u>
+</div>
+<div>&nbsp;</div>
+<div>
+Самой распространенной причиной появления сбоев в операционной системе являются вирусы. В следствии чего у пользователя: появляются систематические ошибки, самопроизвольные перезагрузки, программы начинают зависать,  и самой большой проблемой с которой может столкнутся пользователь - это потеря информации.
+<div>&nbsp;</div>
+Удалить вирус – довольно сложная процедура, с помощью которой трудно будет восстановить правильную работы системы, поэтому чаще всего в таком случае требуется инсталляция, а точнее переустановка Windows Киев.
+<div>&nbsp;</div>
+Другой причиной появления сбоев может быть самостоятельная настройка Windows. Неграмотное разбиение на диски, неправильное форматирование, установка Windows на дому поверх старой, неграмотная настройка Windows - все это может стать причиной некорректной работы и привести к потере информации, которая хранится на вашем компьютере. Вот поэтому установка Windows Киев на ноутбук или стационарный ПК должна осуществляться исключительно знающими и опытными профессионалами.
+<div>&nbsp;</div>
+Еще одной причиной сбоев в работе ОС может быть ее засорение различными файлами, программами и “тяжелыми”фильмами и играми, что приводит к нестабильной работе компьютера, длительным загрузкам или спонтанным перезагрузкам, а также к самопроизвольному выключению. В таком случае рекомендуется обратиться за помощью к специалистам, которые смогут выявить причины таких сбоев и осуществят грамотную настройку или же установку вашей операционной системы.
+<div>&nbsp;</div>
+Наши высококвалифицированные cпециалисты смогут установить на вашем ноутбуке или компьютере любую операционную систему, а также грамотно ее настроят в сжатые сроки и с гарантией.
+<div>&nbsp;</div>
+Производительность и корректность работы программ и системы зависит от того, как и кто осуществил настройку, установку или ремонт вашего компьютера. Только грамотная инсталляция системы Windows, квалифицированный ремонт и верный подбор драйверов и программ гарантируют безотказную и скоростную работу вашего ПК.
+<div>&nbsp;</div>
+Именно специалисты в нашей компании на высоком профессиональном уровне и со знанием всех нюансов грамотно и быстро осуществят установку Windows на новый ноутбук (производится настройка для корректной работы всех устройств, которые подключаются к компьютеру, а также при необходимости выполняется установка Windows 7, 8, XP и даже сразу двух ОС, благодаря чему вы сможете использовать свой компьютер максимально, так как совместите наилучшие качества каждой из установленных ОС).
+<div>&nbsp;</div>
+Установка Windows Киев нашими профессионалами – это гарантия “здоровья” вашего компьютера на долгое время.
+<div>&nbsp;</div>
+http://ruseller.com/lessons.php?rub=43&id=2051
+<div>&nbsp;</div>
+http://htmlbook.ru/samhtml5/formy
+<div>&nbsp;</div>
+http://mediaqueri.es/
+<div>&nbsp;</div>
+https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Media_queries
+<div>&nbsp;</div>
+
+<div class="search"><form>
+<p><label>Найти <input type="search" name="query"</label></p>
+<p><button type="submit">Отправить</button></p>
+</form></div>
+
+<div class="qwer">
+	<form>
+	<fieldset>
+		<p><label>E-mail <input type="e-mail" name="email" title="Это не похоже на допустимый эл-адрес."</label></p>
+		<p><label>Сайт   <input type="url" name="website"</label></p>
+		<p><label>Name  <input type="name" name="name"</label></p>
+		<p><label>Phone  <input type="tel" name="phone"</label></p>
+	</fieldset>
+	<p><button type="submit">Отправить</button></p>
+	</form>
+</div>
+
+<form>
+<fieldset>
+<legend>Назначить встречу</legend>
+<p><label>Дата <input type="date" name="date" min="2011-03-15" max="2012-03-14"></label></p>
+<p><label>Время <input type="time" name="time" min="07:00" max="23:00"></label></p>
+</fieldset>
+<p><button type="submit">Отправить<p></button>
+</form>
+
+<form>
+<p><label>Укажите дату и время <input type="datetime" name="dateAndTime"></label></p>
+<p><button type="submit">Отправить<p></button>
+</form>
+
+<form>
+<p><label>Количество <input type="number" name="quantity"></label></p>
+<p><button type="submit">Отправить<p></button>
+</form>
+
+<form id="auth" action="handler.php" method="post"></form>
+  <p style="color:red">Ввод логина и пароля</p>
+  <p><input name="login" form="auth">
+  <input type="password" name="pass" form="auth"></p>
+  <p><input type="submit" form="auth"></p>
+
+Второй вариант отправки запроса смотри в исх. коде 
+<!--
+(Хотя параметры передачи формы традиционно указываются в теге <form>, их можно перенести и в кнопки отправки формы (<button> и <input type="submit">). Для этого применяется набор атрибутов formaction, formmethod, formenctype и formtarget, которые являются аналогами соответствующих атрибутов без приставки form. В примере 4 показано использование этих атрибутов.)
+
+   <form>
+   <p><input placeholder="Ваше имя" name="user"></p>
+   <p><input type="submit" value="Отправить" 
+    formaction="handler.php" formmethod="post"></p>
+  </form>
+  
+  -->
+  <br/>
+  http://ruseller.com/lessons.php?rub=37&id=169
+  
+  <!-- Вывод случайной цытаты -->
+  <?php
+  $quotes[] = 'Монета, упавшая на дно реки, может показаться рыбам подарком с неба. Что они будут делать с этим подарком?';
+    $quotes[] = 'Если увидишь нечто, к чему стоит приложить руку, поступи не так, как подсказывает разум, но как велит настроение.';
+    $quotes[] = 'Если то, что ты делаешь, трудно тебе - посмотри, нуждаешься ли ты в этом.';
+    $quotes[] = 'Возможно, твои ошибки - это то, что нужно Миру.';
+    $quotes[] = 'Всякий может ударить слабого, но только слабый хочет ударить слабого.';
+ srand ((double) microtime() * 1000000);
+    $random_number = rand(0,count($quotes)-1);
+ echo ($quotes[$random_number]);  
+?>
+  
+  <audio src="Дзідзьо - Сусіди (2013).mp3" controls preload="none">
+Скачать <a href="Дзідзьо - Сусіди (2013).mp3">42 эпизод "Учимся любить HTML5"</a>
+</audio>
+  
+  <audio controls>
+ <source src="audio.ogg">
+<source src="audio.mp3">
+Скачать <a href="audio.ogg">42 эпизод "Учимся любить HTML5"</a>
+</audio>
+  
+  <br />
+  <div><a href="Ремонт ПК.html" class="button"/>Preview</a></div>
+  <br />
+  <br />
+  Сайт с готовыми решениями CSS
+  <br />
+  <br />
+  http://dbmast.ru/vertikalnoe-menyu-navigacii-css-s-podmenyu
+  <br />
+  <br />
+  http://dbmast.ru/sozdaem-mnogourovnevoe-gorizontalnoe-menyu-s-effektom-animacii-css3
+  <br />
+  <br />
+  http://dbmast.ru/raznocvetnye-lentochki-v-uglah-stranicy-s-pomoshhyu-css3
+  <br />
+  <br />
+  Всплывающее окно
+  <br />
+  <br />
+  http://dbmast.ru/sozdaem-vsplyvayushhee-okno-pri-zagruzke-sajta-s-pomoshhyu-css3-i-nemnogo-javascript
+  <br />
+  <br />
+  Лайтбокс эффект с помощью магии CSS3
+  
+  <br />
+  http://dbmast.ru/lajtboks-effekt-s-pomoshhyu-magii-css3
+  
+  <br />
+  <br />
+  <br />
+  <br />
+  http://dbmast.ru/10-urokov-po-sozdaniyu-otlichnyh-menyu-navigacii-dlya-vashih-sajtov
+  <br />
+  <br />
+  <br />
+  Создаем выпадающее меню навигации на CSS3
+  <br />
+  <br />
+  http://dbmast.ru/sozdaem-vypadayushhee-menyu-navigacii-na-css3
+  
+</div>
+</div>
+</main>
+
+
+
+<div class="rightpanel">Рекламма2
+<p>Текст</p>
+<p>Картинка 1</p>
+<p>Картинка 2</p>
+<div><img src="images/04.jpg"></div>
+<div><img src="images/03.jpg"></div>
+
+<p>Опрос</p>
+<p>Какой Виндовс Вы используете</p>
+  <p><input list="character">
+   <datalist id="character">                     <!-- datalist - создает список -->
+    <option value="Windows XP"></option>
+    <option value="Windows 7"></option>
+    <option value="Windows 8.1"></option>
+   </datalist></p>
+     
+</div>
+
+
+
+
+<footer>
+<div>Все права защищены © 2015</div>
+
+<a href="http://www.pcremont.in.ua/установка-windows/">http://www.pcremont.in.ua/установка-windows/</a> 
+
+<div class="footermetric">
+метрика сайта
+</div>
+</footer>
+
+</div>
+
+
+
+<!-- <script src="http://code.jquery.com/jquery-latest.js"></script> -->
+<!-- <script src="js/bootstrap.min.js"></script> -->
+</div>
+
+</body>
+</html>
+
+
